731a40b1f658c408607c24826078446b6254e8a5
David Fernandez <davidefn1991@gmail.com>
2019-09-17 12:25:22 +0100

    first commit

---
+++ src/main/java/dad/maven/hola/Main.java
@@ -0,0 +1,16 @@
+package dad.maven.hola;
+
+import org.apache.commons.lang3.StringUtils;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		String str = StringUtils.reverse("Hola Maven!");
+
+		System.out.println("Hola Maven!");
+		System.out.println(str);
+		
+	}
+
+}
